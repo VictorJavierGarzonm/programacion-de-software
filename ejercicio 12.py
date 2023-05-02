8944ef487db83b20b6b949f2f59427ac5e3c60ef
VictorJavierGarzonm <132398251+VictorJavierGarzonm@users.noreply.github.com>
2023-05-02 16:57:22 -0500

    Add files via upload

---
+++ ejercicio 12.py
@@ -0,0 +1,7 @@
+print("vamos a calcular el area de el cuadrado")
+  print("ingrese el valor de un lado ")
+    numero1=int(input())
+    a=numero1*numero1
+    print("el area de el cuadrado es",a)
+    p=numero1+numero1+numero1+numero1
+    print("el perimetro de el cuadrado es"p)
