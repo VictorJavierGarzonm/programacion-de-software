368785a83ad88ba00a1c38837792857edb6798e3
VictorJavierGarzonm <132398251+VictorJavierGarzonm@users.noreply.github.com>
2023-05-02 16:51:10 -0500

    Add files via upload

---
+++ ejercicio11.py
@@ -0,0 +1,11 @@
+print("calculemos el area del cilindro")
+  print("digite el radio de el cilindro")
+    numero1=float(input())
+    pi=3.1416
+    p=numero1**2
+    a2=pi*p
+    print("el area es de",a2)
+    print("digite la altura de el cilindro")
+    numero2=float(input())
+    altura=a2*n2
+    print("el volumen es",altura)
