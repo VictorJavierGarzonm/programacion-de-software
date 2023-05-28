0e07e311c2811fcc966ef130c9475c42b4b79ecc
VictorJavierGarzonm <132398251+VictorJavierGarzonm@users.noreply.github.com>
2023-05-27 23:27:34 -0500

    Add files via upload

---
+++ miscelania python.py
@@ -0,0 +1,104 @@
+print("ingrese el numero de el ejercicio")
+number=int(input())
+if number==1: 
+   print("ingrese su primer numero")
+   x=int(input())
+   print("Ingrese su segundo numero")
+   y=int(input())
+   suma=x+y
+   print("el resultado es",suma)
+elif number==2:
+  print("ingrese el numero que va a ser potenciado")
+  x=int(input())
+  potencia=x**2
+  print("el numero al cuadrado es",potencia)
+elif number==3:
+  print("suma de 1234 + 532")
+  x=1234
+  y=532
+  suma=x+y
+  print("el resultado es",suma)
+elif number==4:
+  print("resta de 1234-532")
+  x=1234
+  y=532
+  resta=x-y
+  print("el resultado es ",resta)
+
+elif number==5:
+  print("Multiplicacion de 1234 *532")
+  x=1234
+  y=532
+  multiplicacion=x*y
+  print("el resultado es",multiplicacion)
+
+elif number==6:
+  print("division de 1234, 532")
+  x=1234
+  y=532
+  division=x/y
+  print("el resultado es ",division)
+
+elif number==7:
+  print("modulo entre 1234 y 532")
+  x=1234
+  y=532
+  mod=x%y
+  print("el resultado es",mod)
+
+elif  number==8:
+  print("euros a dolares")
+  x=int(input())
+  y=float(1.10)
+  euro=x*y
+  print("el resultado es",euro)
+elif  number==9:
+   print("ingrese la longitud de el rectangulo")
+   x=float(input())
+   y=float(input())
+   area=x*y
+   print("el resultado es",area)
+
+elif number==10:
+   print("ingrese el radio de su cilindro")
+   x=float(input())
+   pi=2,1416
+   p=x**y
+   a=pi*p
+   print("el area es",a)
+   print("ingrese la altura del cilindro")
+   x2=float(input())
+   altura=a*x2
+   print("el volumen es".altura)
+
+elif number==11:
+  print("ingrese el valor de un lado del cuadrado")
+  x=int(input())
+  a=x*x
+  print("el area es",a)
+  p=x+x+x+x
+  print("el perimetro es",p)
+
+elif number==12:
+  print("ingrese el valor del diametro de su circulo")
+  x=float(input())
+  ñ=x/2
+  pi=3.14
+  long=pi*ñ
+  l=p**2
+  a=l*pi
+  print("el radio es ",ñ)
+  print("la longitud es de",long)
+  print("el area de su circulo es de ",a )
+
+elif number==13:
+  print("Ingrese su primer valor")
+  x=float(input())
+  print("ingrese el segundo valor")
+  y=float(input())
+  print("ingrese el tercer valor")
+  a=float(input())
+  suma=x+y+a
+  division=suma/3
+  print("el promedio es de",division)
+  
