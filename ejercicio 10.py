377feb06a584a8c6068fb2a7edd7b7585362ce51
VictorJavierGarzonm <132398251+VictorJavierGarzonm@users.noreply.github.com>
2023-06-04 19:27:35 -0500

    Delete ejercicio 10.py

--- ejercicio 10.py
+++
@@ -1,6 +0,0 @@
-print("ingrese el largo de el rectangulo")
-  numero1=float(input()))
-    print("ingrese el ancho del rectangulo")
-    numero2=float(input())
-    A=numero1*numero2
-    print("el area es ",A)
